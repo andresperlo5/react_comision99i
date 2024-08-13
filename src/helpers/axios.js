fce31cb7a193e4b16928ad02981f5e0f26c692ba
unknown <andresperlo5@gmail.com>
2024-08-12 21:19:48 -0300

    se cambia nombre a la variable de clientAxios

--- src/helpers/axios.js
+++ src/helpers/axios.js
@@ -2,7 +2,7 @@ import axios from "axios";
 const token = JSON.parse(sessionStorage.getItem("token")) || "";
 console.log(token);
 const clientAxios = axios.create({
-  baseURL: `${import.meta.env.VITE_URL_BACK_LOCAL}/api`,
+  baseURL: `${import.meta.env.VITE_URL_BACK_DEPLOY}/api`,
 });
 
 export const configHeader = {
